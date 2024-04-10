4e8e6933902405b01275fa0c0150943a4c1ee5ce
Hamjoshua <biryukov-10@list.ru>
2024-04-10 11:05:15 +0700

    Удаление кубов, цвет меняется при выделении/наведении

--- Unity-Isit-01/Assets/Scripts/CubeScript.cs
+++ Unity-Isit-01/Assets/Scripts/CubeScript.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Material _hoverMaterial;
+    private Material removeHoverMaterial;
+    private Material _defaultMaterial;
+    private Material _selectedMaterial;
+
+    private MeshRenderer _renderer;
+    private bool _isSelected;
+    public bool Selected
     {
-        
+        get
+        {
+            return _isSelected;
+        }
     }
 
-    // Update is called once per frame
+    private void OnMouseOver()
+    {
+        if (!_isSelected)
+        {
+            CursorMode gmCursor = GameManager.SingleInstance.CurrentCursorMode;
+            if (gmCursor == CursorMode.Remove)
+            {
+                _renderer.material = removeHoverMaterial;
+
+            }
+            else if (gmCursor == CursorMode.Select)
+            {
+                _renderer.material = _hoverMaterial;
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (!_isSelected)
+        {
+            _renderer.material = _defaultMaterial;
+        }        
+    }
+
+
+    public void Select()
+    {
+        _isSelected = true;
+        _renderer.material = _selectedMaterial;
+    }
+
+    public void Unselect()
+    {
+        _isSelected = false;
+        _renderer.material = _defaultMaterial;
+    }
+
+    void Awake()
+    {
+        _renderer = gameObject.GetComponent<MeshRenderer>();
+        _hoverMaterial = Resources.Load<Material>("Materials/HoverCubeMaterial");
+        removeHoverMaterial = Resources.Load<Material>("Materials/RemoveHoverCubeMaterial");
+        _selectedMaterial = Resources.Load<Material>("Materials/SelectedCubeMaterial");
+
+        _defaultMaterial = _renderer.material;
+    }
+
     void Update()
     {
-        
+
     }
 }
--- Unity-Isit-01/Assets/Scripts/GameManager.cs
+++ Unity-Isit-01/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 [SerializeField]
@@ -8,20 +9,21 @@ public enum CursorMode
 {
     Select = 0,
     Create = 1,
-    Push = 2
+    Push = 2,
+    Remove = 3
 }
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private CursorMode _cursorMode;
     private GameObject _cubePrefab;
     private GameObject _pushSpherePrefab;
     private GameObject _tempPushSphere;
 
+    UnityEvent _unselectEvent;    
+
     private Transform _objectForPush;
     private Vector3 _pointOfPush;
-    private float _currentPushForce = 0f;    
+    private float _currentPushForce = 0f;
 
     float MaxPushForce = 100f;
     float PushIncreasingSpeed = 0.1f;
@@ -30,6 +32,7 @@ public class GameManager : MonoBehaviour
 
     private static GameManager _singleInstance;
 
+    public CursorMode CurrentCursorMode;
     public static GameManager SingleInstance
     {
         get
@@ -44,16 +47,22 @@ public class GameManager : MonoBehaviour
             return _singleInstance;
         }
     }
-
     public void ChangeCursorMode(int cursorMode)
     {
-        _cursorMode = (CursorMode)cursorMode;
+        CurrentCursorMode = (CursorMode)cursorMode;
     }
 
     void Start()
     {
+        _unselectEvent = new UnityEvent();
         _cubePrefab = Resources.Load<GameObject>("Cube");
-        _pushSpherePrefab = Resources.Load<GameObject>("PushSphere");
+        _pushSpherePrefab = Resources.Load<GameObject>("PushSphere");        
+    }
+
+    void UnselectCube()
+    {
+        _unselectEvent.Invoke();
+        _unselectEvent.RemoveAllListeners();
     }
 
 
@@ -79,34 +88,58 @@ public class GameManager : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100.0f))
             {
                 Transform hittedObject = hit.transform;
-
-                if (_cursorMode == CursorMode.Create && hittedObject.tag == "Plane")
+                if(hittedObject.tag == "Plane")
                 {
-                    Debug.Log($"Cube created on");
-                    Vector3 newPosition = new Vector3(hit.point.x, 1f, hit.point.z);
+                    switch (CurrentCursorMode)
+                    {
+                        case CursorMode.Select:
+                            UnselectCube();
+                            break;
+                        case CursorMode.Create:
+                            Debug.Log($"Cube created on");
+                            Vector3 newPosition = new Vector3(hit.point.x, 1f, hit.point.z);
 
-                    Instantiate(_cubePrefab, newPosition, Quaternion.identity);
+                            Instantiate(_cubePrefab, newPosition, Quaternion.identity);
+                            break;
+                    }                    
                 }
+                
                 if (hittedObject.tag == "Cube")
                 {
-                    if (_cursorMode == CursorMode.Select)
+                    switch (CurrentCursorMode)
                     {
+                        case CursorMode.Select:                            
+                            CubeScript cubeScript = hittedObject.GetComponent<CubeScript>();
+                            bool cubeWasSelected = cubeScript.Selected;
 
-                    }
+                            UnselectCube();
 
-                    if (_cursorMode == CursorMode.Push)
-                    {
-                        _objectForPush = hittedObject;
-                        _pointOfPush = hit.point;
-                        StartCoroutine(IncreasePushForse());
+                            // Если куб уже был выбран, то по нажатию мы отменяем выделение, чтобы он не выделился еще раз
+                            if (cubeWasSelected)
+                            {                                
+                                return;
+                            }
+                            cubeScript.Select();
+                            _unselectEvent.AddListener(cubeScript.Unselect);
+                            break;
+
+                        case CursorMode.Push:
+                            _objectForPush = hittedObject;
+                            _pointOfPush = hit.point;
+                            StartCoroutine(IncreasePushForse());
+                            break;
+
+                        case CursorMode.Remove:
+                            Destroy(hittedObject.gameObject);
+                            break;
                     }
                 }
             }
         }
 
-        if (Input.GetMouseButtonUp(0) && _cursorMode == CursorMode.Push)
+        if (Input.GetMouseButtonUp(0) && CurrentCursorMode == CursorMode.Push)
         {
-            Rigidbody rbOfPushedObject = _objectForPush.GetComponent<Rigidbody>();            
+            Rigidbody rbOfPushedObject = _objectForPush.GetComponent<Rigidbody>();
             rbOfPushedObject.AddForceAtPosition(Vector3.forward * _currentPushForce, _pointOfPush);
 
             Destroy(_tempPushSphere);
@@ -114,7 +147,7 @@ public class GameManager : MonoBehaviour
             StopAllCoroutines();
             _currentPushForce = 0f;
         }
-    }    
+    }
 
     IEnumerator IncreasePushForse()
     {
