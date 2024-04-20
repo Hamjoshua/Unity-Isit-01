0c6f6579a492ca1b7c3242c4a6c8881d0660cbbe
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2024-04-20 10:25:20 +0700

    Добавлены сцены

---
+++ Unity-Isit-01/Assets/Lab04/DetectGhosts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectGhosts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string info = $"GameObjcet \"{other.gameObject.name}\" has fallen";
+        Debug.Log(info);
+    }
+}
--- Unity-Isit-01/Assets/Scripts/CameraScript.cs
+++ Unity-Isit-01/Assets/Scripts/CameraScript.cs
@@ -45,8 +45,4 @@ public class CameraScript : MonoBehaviour
         }
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(GameManager.SingleInstance);
-    }    
 }
